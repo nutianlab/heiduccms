3deebbdf71afbfe6cf4b11e719b08dcbd2c3128e
Tan <tamsiupo@gmail.com>
2015-07-01 03:16:22 +0000

    添加页面模版php语法支持

--- kernel/src/main/java/com/heiduc/business/page/impl/SimplePageRenderDecorator.java
+++ kernel/src/main/java/com/heiduc/business/page/impl/SimplePageRenderDecorator.java
@@ -1,5 +1,3 @@
-
-
 package com.heiduc.business.page.impl;
 
 import org.apache.velocity.VelocityContext;
@@ -14,14 +12,11 @@ import com.heiduc.i18n.Messages;
 /**
  * 
  * @author Alexander Oleynik
- *
+ * 
  */
 public class SimplePageRenderDecorator extends AbstractPageRenderDecorator {
 
-	public SimplePageRenderDecorator(PageEntity page,	String languageCode, 
-			Dao dao,
-			PageBusiness pageBusiness, 
-			SystemService systemService) {
+	public SimplePageRenderDecorator(PageEntity page, String languageCode, Dao dao, PageBusiness pageBusiness, SystemService systemService) {
 		super();
 		setPage(page);
 		setLanguageCode(languageCode);
@@ -32,26 +27,27 @@ public class SimplePageRenderDecorator extends AbstractPageRenderDecorator {
 	}
 
 	private void prepareContent() {
-		ContentEntity contentEntity = getPageBusiness().getPageContent(
-				getPage(), getLanguageCode());
+		ContentEntity contentEntity = getPageBusiness().getPageContent(getPage(), getLanguageCode());
 		if (contentEntity == null) {
-			String msg = Messages.get("content_not_found") + " " 
-					+ getFriendlyURL() + " " + getLanguageCode();
+			String msg = Messages.get("content_not_found") + " " + getFriendlyURL() + " " + getLanguageCode();
 			logger.error(msg);
 			setContent(msg);
 			return;
 		}
 		String resultContent = contentEntity.getContent();
 		if (isVelocityProcessing()) {
-			VelocityContext context = getPageBusiness().createContext(
-					getLanguageCode(), getPage());
+			VelocityContext context = getPageBusiness().createContext(getLanguageCode(), getPage());
 			context.put("page", getPage());
 			resultContent = getSystemService().render(resultContent, context);
 		}
 		if (isWikiProcessing()) {
-			resultContent = getSystemService().renderWiki(resultContent, 
-					getPage());
+			resultContent = getSystemService().renderWiki(resultContent, getPage());
 		}
+
+		if (isPHPProcessing()) {
+			resultContent = getSystemService().renderPHP(resultContent, getPage());
+		}
+
 		setContent(resultContent);
 	}
 
