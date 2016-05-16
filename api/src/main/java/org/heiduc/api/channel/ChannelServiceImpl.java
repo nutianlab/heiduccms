1822b77c632236026b5495a288849947d0fcc4e7
txb <dev@heiduc.com@f0695f36-db1c-416a-948d-9e1acc1af17a>
2016-05-16 05:05:41 +0000

    处理内容未初始化情况下的情况
    Signed-off-by: txb <dev@heiduc.com>

--- api/src/main/java/org/heiduc/api/channel/ChannelServiceImpl.java
+++ api/src/main/java/org/heiduc/api/channel/ChannelServiceImpl.java
@@ -3,11 +3,15 @@ package org.heiduc.api.channel;
 import org.atmosphere.cpr.Broadcaster;
 import org.atmosphere.cpr.BroadcasterFactory;
 import org.atmosphere.cpr.DefaultBroadcaster;
+import org.atmosphere.cpr.DefaultBroadcasterFactory;
 
 
 public class ChannelServiceImpl implements ChannelService {
 
+	private BroadcasterFactory broadcasterFactory  = null;
+	
 	ChannelServiceImpl(){
+		broadcasterFactory = new DefaultBroadcasterFactory();
 	}
 	
 	@Override
@@ -17,7 +21,8 @@ public class ChannelServiceImpl implements ChannelService {
 
 	@Override
 	public void sendMessage(ChannelMessage channelMessage) {
-		Broadcaster broadcaster  = BroadcasterFactory.getDefault().lookup(DefaultBroadcaster.class,"/_ah/channel/"+channelMessage.getClientId());
+		
+		Broadcaster broadcaster  = broadcasterFactory.lookup(DefaultBroadcaster.class,"/_ah/channel/"+channelMessage.getClientId());
 		broadcaster.broadcast(channelMessage.getMessage());
 	}
 
