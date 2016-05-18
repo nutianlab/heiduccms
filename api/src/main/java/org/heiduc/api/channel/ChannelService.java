0d915c8d331ac7cba6d05bbaab1f02665955e5eb
txb <dev@heiduc.com@f0695f36-db1c-416a-948d-9e1acc1af17a>
2016-05-18 03:15:48 +0000

    升级atmosphere-runtime至2.4.3
    Signed-off-by: txb <dev@heiduc.com>

--- api/src/main/java/org/heiduc/api/channel/ChannelService.java
+++ api/src/main/java/org/heiduc/api/channel/ChannelService.java
@@ -1,9 +1,13 @@
 package org.heiduc.api.channel;
 
+import org.atmosphere.cpr.BroadcasterFactory;
+
 public interface ChannelService {
 
 	void sendMessage(ChannelMessage channelMessage);
 
 	String createChannel(String clientId);
+	
+	void setBroadcasterFactory(BroadcasterFactory broadcasterFactory);
 
 }
