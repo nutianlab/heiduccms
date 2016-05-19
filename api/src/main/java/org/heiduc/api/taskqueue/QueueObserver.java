7761ac4d155a9fd76b4efb927f1eea2a57975cf9
txb <dev@heiduc.com@f0695f36-db1c-416a-948d-9e1acc1af17a>
2016-05-19 04:46:10 +0000

    添加日志信息
    Signed-off-by: txb <dev@heiduc.com>

--- api/src/main/java/org/heiduc/api/taskqueue/QueueObserver.java
+++ api/src/main/java/org/heiduc/api/taskqueue/QueueObserver.java
@@ -2,27 +2,24 @@ package org.heiduc.api.taskqueue;
 
 import java.util.Observable;
 import java.util.Observer;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.RejectedExecutionHandler;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class QueueObserver implements Observer {
+	
+	private  static final Logger logger = LoggerFactory.getLogger(QueueObserver.class);
 
 	@Override
 	public void update(Observable o, Object arg) {
-//		System.out.println("============update in============");
-//		System.out.println("已运行的线程数"+Thread.activeCount());
-		
+		logger.info("已运行的线程数"+Thread.activeCount());
 		TaskOptions task = (TaskOptions)arg;
-//		System.out.println("taskNmae is "+task.getTaskName());
-//		System.out.println("开始初始化..................");
+		logger.info("taskNmae is "+task.getTaskName());
+		logger.info("开始初始化..................");
 		CustomThreadPoolExecutor executor = ThreadPool.getInstance();
-//		System.out.println("调用线程执行");
+		logger.info("调用线程执行");
         executor.execute(new TaskThread(task));
-//        System.out.println("线程池运行的线程数"+executor.getActiveCount());
-//		System.out.println("============update out============");
+		logger.info("线程池运行的线程数"+executor.getActiveCount());
 	}
 
 }
