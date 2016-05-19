4353f40c4e4785fc9798d595b341043a28b3e2de
txb <dev@heiduc.com@f0695f36-db1c-416a-948d-9e1acc1af17a>
2016-05-19 04:45:40 +0000

    消除警告
    Signed-off-by: txb <dev@heiduc.com>

--- api/src/main/java/org/heiduc/api/taskqueue/QueueFactory.java
+++ api/src/main/java/org/heiduc/api/taskqueue/QueueFactory.java
@@ -5,17 +5,15 @@ import java.util.Map;
 
 public class QueueFactory {
 
-	private static final Map<String, Queue> queueMap = new HashMap();
+	private static final Map<String, Queue> queueMap = new HashMap<String, Queue>();
 	
 	public static Queue getDefaultQueue() {
 		return getQueue("default");
 	}
 
 	public static Queue getQueue(String queueName) {
-//		System.out.println("=========QueueFactory getQueue=========");
 		Queue queue = (Queue)queueMap.get(queueName);
 	    if (queue == null) {
-//	      System.out.println("=========queue is null=========");
 	      queue = new QueueImpl(queueName);
 	      queueMap.put(queueName, queue);
 	    }
