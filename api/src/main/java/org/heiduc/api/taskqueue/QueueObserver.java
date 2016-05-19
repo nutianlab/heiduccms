aa961f4c5a39a43ee43c133d122cf08c952e5484
txb <dev@heiduc.com@f0695f36-db1c-416a-948d-9e1acc1af17a>
2016-05-19 06:26:56 +0000

    日志处理
    Signed-off-by: txb <dev@heiduc.com>

--- api/src/main/java/org/heiduc/api/taskqueue/QueueObserver.java
+++ api/src/main/java/org/heiduc/api/taskqueue/QueueObserver.java
@@ -3,23 +3,15 @@ package org.heiduc.api.taskqueue;
 import java.util.Observable;
 import java.util.Observer;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class QueueObserver implements Observer {
 	
-	private  static final Logger logger = LoggerFactory.getLogger(QueueObserver.class);
+//	private  static final Logger logger = LoggerFactory.getLogger(QueueObserver.class);
 
 	@Override
 	public void update(Observable o, Object arg) {
-		logger.info("已运行的线程数"+Thread.activeCount());
 		TaskOptions task = (TaskOptions)arg;
-		logger.info("taskNmae is "+task.getTaskName());
-		logger.info("开始初始化..................");
 		CustomThreadPoolExecutor executor = ThreadPool.getInstance();
-		logger.info("调用线程执行");
         executor.execute(new TaskThread(task));
-		logger.info("线程池运行的线程数"+executor.getActiveCount());
 	}
 
 }
